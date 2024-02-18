3419dbf3c183d0d40df9b6b7e5b43d715a0f990b
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2024-02-18 13:10:30 -0500

    changes

--- index.js
+++ index.js
@@ -5,5 +5,28 @@
 //       this.update = this.update.bind(this);
 //    }
 
-//    setText(newText)
+//    setText(newText) {
+//       const oldText = this.element.innerText;
+//       const length = Math.max(oldText.length, newText.length);
+//       const promise = new Promise((resolve) => this.resolve = resolve);
+//       this.queue = []
+
+//       for (let i = 0; i < length; i++) {
+//          const from = oldText[i] || '';
+//          const to = newText[i] || '';
+
+//          const start = Math.floor(Math.random() * 50);
+//          const end = start + Math.floor(Math.random() * 50);
+
+//          this.queue.push({
+//             from,
+//             to,
+//             start,
+//             end
+//          });
+//       }
+
+//       cancelAnimationFrame(this.frameRequest);
+//       this.frame = 0;
+//    }
 // }
