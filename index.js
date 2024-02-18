a7d3ac48967605b95e09d2997e9d1e13a73b7144
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2024-02-17 23:10:34 -0500

    text shuffle

---
+++ index.js
@@ -0,0 +1,9 @@
+// class Shuffle {
+//    constructor(element) {
+//       this.element = element;
+//       this.characters = '01';
+//       this.update = this.update.bind(this);
+//    }
+
+//    setText(newText)
+// }
